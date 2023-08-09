cf1d5b1a682eca8f0a6e31a57072e9ef70c7c69a
NazarLyubchinsky <nazar.lyubchinskyi@gmail.com>
2023-08-09 20:28:26 +0300

    adapt

--- src/components/ContactUs/SendMessage/SendMessage.jsx
+++ src/components/ContactUs/SendMessage/SendMessage.jsx
@@ -51,50 +51,58 @@ const SendMessage = () => {
 				<div className="box__contact">
 					<div className="overlap">
 						<form className="form" onSubmit={handleSubmit}>
-							<div className="form__text">
-								<div className="text-wrapper">CONTACT US</div>
-								<h1 className="subtext">Get in Touch</h1>
-							</div>
-							<p className="form__question">
-								We're always happy to hear from our clients and potential clients. Whether you
-								have a question, want to discuss  project, or just want to say hello, we're here to help.
-							</p>
-							<div className="field__name">
-								<div className="overlap-group">
-									<input className="overlap__group-input" placeholder='First Name' required
-										value={firstName}
-										onChange={(e) => setFirstName(e.target.value)} />
+							<div className='form__wrapper'>
+								<div className="form__text">
+									<div className="text-wrapper">CONTACT US</div>
+									<h1 className="subtext">Get in Touch</h1>
+								</div>
+								<p className="form__question">
+									We're always happy to hear from our clients and potential clients. Whether you
+									have a question, want to discuss  project, or just want to say hello, we're here to help.
+								</p>
+								<div className='field'>
+									<div className='field__input'>
+										<div className="field__name">
+											<div className="overlap-group">
+												<input className="overlap__group-input" placeholder='First Name' required
+													value={firstName}
+													onChange={(e) => setFirstName(e.target.value)} />
+											</div>
+										</div>
+										<div className="field__lstname">
+											<div className="overlap-group">
+												<input className="overlap__group-input" placeholder='Last Name' required
+													value={lastName}
+													onChange={(e) => setLastName(e.target.value)} />
+											</div>
+										</div>
+										<div className="field__email">
+											<div className="overlap-group">
+												<input className="overlap__group-input" placeholder='Email' required
+													value={email}
+													onChange={(e) => setEmail(e.target.value)} />
+											</div>
+										</div>
+										<div className="field__number">
+											<div className="overlap-group">
+												<input className="overlap__group-input" placeholder='Phone Number' required
+													value={phoneNumber}
+													onChange={(e) => setPhoneNumber(e.target.value)} />
+											</div>
+										</div>
+									</div>
+									<div className="field-2">
+										<div className="overlap-2">
+											<textarea className="textarea__message" placeholder='Message' required
+												value={message}
+												onChange={(e) => setMessage(e.target.value)}></textarea>
+										</div>
+									</div>
+									<div className='send__button'>
+										<button className="send-message">SEND MESSAGE</button>
+									</div>
 								</div>
 							</div>
-							<div className="field__lstname">
-								<div className="overlap-group">
-									<input className="overlap__group-input" placeholder='Last Name' required
-										value={lastName}
-										onChange={(e) => setLastName(e.target.value)} />
-								</div>
-							</div>
-							<div className="field__email">
-								<div className="overlap-group">
-									<input className="overlap__group-input" placeholder='Email' required
-										value={email}
-										onChange={(e) => setEmail(e.target.value)} />
-								</div>
-							</div>
-							<div className="field__number">
-								<div className="overlap-group">
-									<input className="overlap__group-input" placeholder='Phone Number' required
-										value={phoneNumber}
-										onChange={(e) => setPhoneNumber(e.target.value)} />
-								</div>
-							</div>
-							<div className="field-2">
-								<div className="overlap-2">
-									<textarea className="textarea__message" placeholder='Message' required
-										value={message}
-										onChange={(e) => setMessage(e.target.value)}></textarea>
-								</div>
-							</div>
-							<button className="send-message">SEND MESSAGE</button>
 						</form>
 						<div className="image">
 							<div className="overlap-3">
@@ -110,9 +118,9 @@ const SendMessage = () => {
 											<img className="big__image" alt="big__image" src={bigImage} />
 										</div>
 									</div>
-							<div className="logo-instagram-wrapper"></div>
-								<div className="logo-fb-simple-wrapper"></div>
-							<div className="logo-telegram-wrapper"></div>
+									<div className="logo-instagram-wrapper"></div>
+									<div className="logo-fb-simple-wrapper"></div>
+									<div className="logo-telegram-wrapper"></div>
 								</div>
 							</div>
 						</div>
