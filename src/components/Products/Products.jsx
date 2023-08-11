c1b6683fe471d461ef59bf7c844bb078de48eaae
NazarLyubchinsky <nazar.lyubchinskyi@gmail.com>
2023-08-11 17:20:36 +0300

    fix

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,30 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
+// import Preloader from "../Preloader/Preloader";
 
 import s from "./index.module.scss";
 
-const Products = ({ title,  products = [], amount }) => {
+const Products = ({ title, products = [], amount }) => {
 
 	const list = products.filter((_, i) => i < amount);
 
 	return (
 		<div className={s.products} >
 			{title && <h2 className={s.title}>{title}</h2>}
+
+
 			<div className={s.list}>
-				{list.map(({ id, images, title, category: { name: cat } }) => (
-					<Link to={`/products/${id}`} key={id} className={s.product}>
-						<div className={s.box}>
-							<div className={s.group}>
-								<div className={s.overlap__group} style={{ backgroundImage: `url(${images[0]})`, }}>
-									<div className={s.overlap}>
-										<div className={s.text__wrapper}>{title}</div>
-										<div className={s.div}>{cat}</div>
+				{
+					list ? (
+						// <Preloader />
+						<p style={{
+							display: 'flex',
+							justifyContent: 'center',
+							width: '100%',
+						}}>	No results</p>
+					) : (
+						list.map(({ id, images, title, category: { name: cat } }) => (
+							<Link to={`/products/${id}`} key={id} className={s.product}>
+								<div className={s.box}>
+									<div className={s.group}>
+										<div className={s.overlap__group} style={{ backgroundImage: `url(${images[0]})`, }}>
+											<div className={s.overlap}>
+												<div className={s.text__wrapper}>{title}</div>
+												<div className={s.div}>{cat}</div>
+											</div>
+										</div>
 									</div>
 								</div>
-							</div>
-						</div>
-					</Link>
-				))}
+							</Link>
+						))
+
+					)
+				}
+
 			</div>
 
 		</div>
